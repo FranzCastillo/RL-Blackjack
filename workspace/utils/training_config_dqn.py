fcf9331d95fa3c7f13a79b3cbff61e81ba79aebf
Diego-Lemus <deloz.lemus.09@gmail.com>
2025-11-11 18:21:44 -0600

    [feat]: modification in training params

--- workspace/utils/training_config_dqn.py
+++ workspace/utils/training_config_dqn.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class TrainingConfigDQN:
     learning_rate: float = 1e-3
-    n_episodes: int = 200_000
+    n_episodes: int = 500_000
     start_epsilon: float = 1.0
     final_epsilon: float = 0.05
     discount_factor: float = 1.0
@@ -13,7 +13,7 @@ class TrainingConfigDQN:
     replay_size: int = 10_000
     target_update_freq: int = 1000
     learn_every: int = 1
-    dqn_hidden: tuple = (64, 64)
+    dqn_hidden: tuple = (32, 32)
 
     @property
     def epsilon_decay(self) -> float:
--- workspace/utils/training_config_tabular.py
+++ workspace/utils/training_config_tabular.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class TrainingConfigTabular:
-    learning_rate: float = 0.1
-    n_episodes: int = 200_000
+    learning_rate: float = 1e-3
+    n_episodes: int = 500_000
     start_epsilon: float = 1.0
     final_epsilon: float = 0.05
     discount_factor: float = 1.0
